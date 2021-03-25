2a9cb2f17c1b7cc1362ba431c30bc77511e9a95d
menshickow <menshickowserezha@yandex.ru>
2021-03-25 15:46:49 +0300

    Added API request to get weather data

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
 import * as Location from "expo-location";
+import { Alert } from "react-native";
+import axios from "axios";
 
 import { Loading } from "./Loading";
 
+const API_KEY = "f5689f44829f49f2a6df29eccb0db9d5";
+
 export default class extends Component {
+  state = {
+    isLoading: true,
+  };
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log("-!-debug-!-", "data", data); // eslint-disable-line
+  };
+
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log("-!-debug-!-", location); // eslint-disable-line
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert("Не могу определить местоположение");
+    }
   };
 
   componentDidMount() {
@@ -14,6 +37,6 @@ export default class extends Component {
   }
 
   render() {
-    return <Loading />;
+    return this.state.isLoading ? <Loading /> : null;
   }
 }
